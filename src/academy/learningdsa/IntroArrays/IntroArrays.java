318234de5c0d6aefc0edd84283296a8c068f7ec4
urielsmp128 <urielsmp128@outlook.com>
2024-02-29 17:26:52 -0600

    Refresher on BubbleSort Algo

--- src/academy/learningdsa/IntroArrays/IntroArrays.java
+++ src/academy/learningdsa/IntroArrays/IntroArrays.java
@@ -2,6 +2,25 @@ package academy.learningdsa.IntroArrays;
 
 public class IntroArrays {
     public static void main(String[] args) {
-        System.out.println("hello!");
+
+        //Declaring an Integer array
+        int[] intArray = new int[7];
+        int[] intArray2 = new int[]{10, 20,30,40, 50, 60, 70};
+
+        //Adding integer values to the array
+        intArray[0] = 20;
+        intArray[1] = 38;
+        intArray[2] = 55;
+        intArray[3] = 99;
+        intArray[4] = 13;
+        intArray[5] = 28;
+        intArray[6] = -100;
+
+        for (int j : intArray) {
+            System.out.println(j);
+        }
+        for (int j : intArray2) {
+            System.out.println(j);
+        }
     }
 }
---
+++ src/academy/learningdsa/SortingAlgos/BubbleSort.java
@@ -0,0 +1,32 @@
+package academy.learningdsa.SortingAlgos;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+
+    public static void swap(int[] array, int i, int j){
+        if (i == j){
+           return;
+        }
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    public static void main(String[] args) {
+
+        int[] intArray = {20, 35, -15, 7, 55, 1, 22};
+
+        for (int lastUnsortedIndex = intArray.length -1; lastUnsortedIndex > 0; lastUnsortedIndex--){
+            for (int i = 0; i < lastUnsortedIndex; i++){
+                if (intArray[i] > intArray[i+1]){
+                    swap(intArray, i, i+1);
+                }
+            }
+        }
+
+        String sortedArray = Arrays.toString(intArray);
+        System.out.println(sortedArray);
+
+    }
+}
