8d7bb9f08ba7149bbe5dad230085d250a2735cb3
urielsmp128 <urielsmp128@outlook.com>
2024-04-22 22:52:28 -0600

    Recursion Intro

---
+++ src/academy/learningdsa/SortingAlgos/Recursion.java
@@ -0,0 +1,32 @@
+package academy.learningdsa.SortingAlgos;
+
+public class Recursion {
+
+    public static void main(String[] args) {
+
+        System.out.println(Recursion.recursiveFactorial(6));
+        System.out.println(Recursion.iterariveFactorial(6));
+
+    }
+
+    public static int iterariveFactorial(int num){
+        if(num == 0 || num == 1){
+            return 1;
+        }
+
+        int factorial = 1;
+        for(int i = 1; i <= num; i++){
+            factorial *= i;
+        }
+
+        return factorial;
+    }
+
+    public static int recursiveFactorial(int num){
+        if (num == 0  || num == 1){
+            return 1;
+        }else {
+            return num * recursiveFactorial(num - 1);
+        }
+    }
+}
