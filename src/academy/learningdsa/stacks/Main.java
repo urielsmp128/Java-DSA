9b2a594d540ce02f8e5b77317aa55117fc99376f
urielsmp128 <urielsmp128@outlook.com>
2024-05-03 17:00:25 -0600

    Testing functionality of our stack class

--- src/academy/learningdsa/stacks/Main.java
+++ src/academy/learningdsa/stacks/Main.java
@@ -1,4 +1,23 @@
 package academy.learningdsa.stacks;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayStack arrayStack = new ArrayStack(10);
+
+        arrayStack.push(new Employee("Jane", "Jones", 123));
+        arrayStack.push(new Employee("John", "Doe", 4567));
+        arrayStack.push(new Employee("Mary", "Smith", 22));
+        arrayStack.push(new Employee("Mike", "Wilson", 3245));
+        arrayStack.push(new Employee("Bill", "End", 78));
+
+        arrayStack.printStack();
+        System.out.println("\n" + arrayStack.peek());
+        arrayStack.pop();
+        System.out.println("\n" + arrayStack.peek());
+        arrayStack.pop();
+        System.out.println("\n" + arrayStack.peek());
+        System.out.println(arrayStack.getSize());
+    }
 }
