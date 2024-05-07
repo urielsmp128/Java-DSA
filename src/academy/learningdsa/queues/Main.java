5ecd91e327f1b65319686ad3686e0d3f7875b3af
urielsmp128 <urielsmp128@outlook.com>
2024-05-06 18:04:39 -0600

    Finished array based queue implementation

--- src/academy/learningdsa/queues/Main.java
+++ src/academy/learningdsa/queues/Main.java
@@ -4,5 +4,24 @@ public class Main {
 
     public static void main(String[] args) {
 
+        ArrayQueue arrayQueue = new ArrayQueue(10);
+
+        arrayQueue.add(new Employee("Jane", "Jones", 123));
+        arrayQueue.add(new Employee("John", "Doe", 4567));
+        arrayQueue.add(new Employee("Mary", "Smith", 22));
+        arrayQueue.add(new Employee("Mike", "Wilson", 3245));
+        arrayQueue.add(new Employee("Uriel", "Madrigal", 1818));
+
+        arrayQueue.remove();
+        arrayQueue.remove();
+        arrayQueue.remove();
+        arrayQueue.remove();
+        arrayQueue.remove();
+
+        arrayQueue.add(new Employee("Jane", "Jones", 123));
+
+        arrayQueue.printQueue();
+
+
     }
 }
