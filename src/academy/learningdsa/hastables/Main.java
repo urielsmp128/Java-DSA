cc72dc99e1b0b7103b93f9789588a71a3d90fd8f
urielsmp128 <urielsmp128@outlook.com>
2024-05-10 17:42:29 -0600

    Implementing basic hashtable without collision management

--- src/academy/learningdsa/hastables/Main.java
+++ src/academy/learningdsa/hastables/Main.java
@@ -4,5 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Employee janeJones = new Employee("Jane", "Jones", 123);
+        Employee johnDoe = new Employee("John", "Doe", 4567);
+        Employee marySmith = new Employee("Mary", "Smith", 22);
+        Employee mikeWilson = new Employee("Mike", "Wilson", 3245);
+
+        SimpleHashtable ht = new SimpleHashtable();
+
+        ht.put("Jones", janeJones);
+        ht.put("Doe", johnDoe);
+        ht.put("Wilson", mikeWilson);
+        ht.put("Smith", marySmith);
+
+        ht.printHashtable();
     }
 }
--- src/academy/learningdsa/hastables/SimpleHashtable.java
+++ src/academy/learningdsa/hastables/SimpleHashtable.java
@@ -1,4 +1,36 @@
 package academy.learningdsa.hastables;
 
 public class SimpleHashtable {
+
+    private Employee[] hashtable;
+
+    public SimpleHashtable(){
+        hashtable = new Employee[10];
+    }
+
+    private int hashKey(String key){
+        return key.length() % hashtable.length;
+    }
+
+    public void put(String key, Employee employee){
+        int hashedKey = hashKey(key);
+        if (hashtable[hashedKey] != null){
+            System.out.println("Srry, there's already an employee at position " + hashedKey);
+        }else {
+            hashtable[hashedKey] = employee;
+        }
+    }
+
+    public Employee get(String key){
+        int hashedKey = hashKey(key);
+        return hashtable[hashedKey];
+    }
+
+    public void printHashtable(){
+        for(Employee employee: hashtable){
+            System.out.println(employee);
+        }
+    }
+
+
 }
